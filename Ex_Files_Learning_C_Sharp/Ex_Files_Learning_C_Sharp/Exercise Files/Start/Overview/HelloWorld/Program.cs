2bbf99f8dcd3fc5d3c4aef18f4083100ae61f11d
Ralph Angelo Fajardo <ralph.fajardo@quantrics.com>
2022-01-24 16:41:49 +0800

    init

---
+++ Ex_Files_Learning_C_Sharp/Ex_Files_Learning_C_Sharp/Exercise Files/Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.WriteLine("Hello World!");
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();
+System.Console.WriteLine("Hello World " +str);
+
---
+++ Ex_Files_Learning_C_Sharp/Ex_Files_Learning_C_Sharp/Exercise Files/Start/Overview/Operators/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare some variables to excercise the operators
+            int x=10, y=5;
+            string a="abcd", b="efgh";
+
+            // TODO: Basic math operators are +, -, /, *
+            // Console.WriteLine("----- Basic Math -----");
+            // System.Console.WriteLine((x / y) * x);
+            // System.Console.WriteLine( a+ b);
+
+            // TODO: Increment / decrement operators
+            Console.WriteLine("----- Shorthand -----");
+            x++;
+            y--;
+            // System.Console.WriteLine(x);
+            // System.Console.WriteLine(y);
+
+            // TODO: Operators can be shorthand: a = a + b
+            // a += b;
+            // System.Console.WriteLine(a);
+
+            // TODO: Logical operators &&, ||
+            // Console.WriteLine("----- Logic Operators -----");
+            System.Console.WriteLine(x > y && y>= 5);
+
+
+            // null-coalescing operators
+            // string str = null;
+            // TODO: the ?? operator uses left operand if not null, or right one if it is
+
+            
+            // TODO: the ??= operator assigns the right operand if the left one is null
+            // it replaces the code:
+            // if (variable is null) {
+            //    variable = somevalue;
+            // }
+
+        }
+    }
+}
