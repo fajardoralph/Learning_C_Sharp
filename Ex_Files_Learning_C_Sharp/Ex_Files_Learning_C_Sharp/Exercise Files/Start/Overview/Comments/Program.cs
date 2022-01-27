e7df4a56ebf7a4411095f8f0f4ef44d5e1ad9528
Ralph Angelo Fajardo <ralph.fajardo@quantrics.com>
2022-01-27 19:45:52 +0800

    ATM

--- Ex_Files_Learning_C_Sharp/Ex_Files_Learning_C_Sharp/Exercise Files/Start/Overview/Comments/Program.cs
+++ Ex_Files_Learning_C_Sharp/Ex_Files_Learning_C_Sharp/Exercise Files/Start/Overview/Comments/Program.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 
-namespace Comments
+    namespace Comments
 {
     class Program
     {
         /// XML Comments are used to help provide documentation
         /// They start with triple-slashes and have a special syntax
+        ///<summary> 
+        ///This is the main sample application function
+        ///</summary>
+        
         static void Main(string[] args)
         {
+            // Single line comments starts with two slashes
+            // You can have as may as these as you want
 
             Console.WriteLine("Hello World!");
 
+            /*
+            Multiple line comments starts with a slash and a star
+            and can continue for several lines
+            until a closing star and slash are encountered.
+            */
         }
     }
 }
